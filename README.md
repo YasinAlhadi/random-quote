e3eb81148349dc386a123936e2266fe481b03a90
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-11-19 17:25:38 +0400

    Create Random quote generator

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# random-quote
+# Random Quote Generator
+
+[![Build Status](https://travis-ci.org/robertdebock/ansible-role-random_quote_generator.svg?branch=master)](https://travis-ci.org/robertdebock/ansible-role-random_quote_generator)
 Generate and share random quote
+
---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.scss'
+
+const baseURL = 'https://api.quotable.io/random'
+
+function App() {
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+  /* const [color, setColor] = SetState('#000000') */
+
+  const getQuote = () => {
+    axios
+      .get(baseURL)
+      .then((res) => {
+        setQuote(res.data.content)
+        setAuthor(res.data.author)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  if (!quote) {
+    getQuote()
+  }
+  return (
+    <>
+    <div className='random-quote'>
+      <div className="heading"> Random Quote Generator</div>
+      <div className="decription">
+        <p>Click on New Quote button to generate random quote</p>
+      </div>
+    </div>
+      <div id='quote-box'>
+        <div id='text'>{quote}</div>
+        <div id='author'>Author: {author}</div>
+        <div id='buttons'>
+        <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`} target='_too'>Tweet Quote</a>
+          <button id='new-quote' onClick={getQuote}>New Quote</button>
+        </div>
+      </div>
+      <div className="links">
+        Created by: <a href="#"> <img src="square-x-twitter.svg" alt="square-x-twitter" /> Yasin Alhadi</a> | Source Code: <a href="#"> <img src="github.svg" alt="github" /> Github</a>
+      </div>
+    </>
+  )
+}
+
+export default App
